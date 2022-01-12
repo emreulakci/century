05c0192a52cad5e00235eb1c517735e27e494423
emreulakci <72080698+emreulakci@users.noreply.github.com>
2022-01-12 23:30:12 +0100

    app.js updated again

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("deneme")
+const yearTocentury = year => {
+    if (typeof year == 'string') {
+        if (year.toString().slice(-2) == '00') {
+            return year.toString().slice(0, 2);
+        } else {
+            return (Math.floor(+year / 100) + 1).toString();
+        };
+    } else if (typeof year == 'number') {
+        return Math.floor((year - 1) / 100) + 1;
+    } else {
+        return undefined;
+    };
+};
+console.log(yearTocentury(20));
